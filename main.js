15d6812042ae01f6fd266465380569bba3bbb0f7
shile25 <96479870+shile25@users.noreply.github.com>
2022-04-15 03:34:46 -0700

    Add game

---
+++ main.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomSourceImage = "image/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomSourceImage);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomSourceImage2 = "image/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src" , randomSourceImage2);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML ="player1 wins"
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML ="player 2 wins"
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw"
+}
